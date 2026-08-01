713a66bcb836d03fd431d5c6e46ee1ae68d2ba82
Anish Prakash <anish1.prakash@gmail.com>
2026-08-01 08:39:46 +0530

    Time: 0 ms (100%), Space: 8.5 MB (79.49%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.c
@@ -0,0 +1,18 @@
+bool predictTheWinner(int* nums, int numsSize) {
+    int dp[20];
+    
+    for (int i = 0; i < numsSize; i++) {
+        dp[i] = nums[i];
+    }
+    
+    for (int len = 2; len <= numsSize; len++) {
+        for (int i = 0; i <= numsSize - len; i++) {
+            int j = i + len - 1;
+            int pickFirst = nums[i] - dp[i + 1];
+            int pickLast = nums[j] - dp[i];
+            dp[i] = pickFirst > pickLast ? pickFirst : pickLast;
+        }
+    }
+    
+    return dp[0] >= 0;
+}
