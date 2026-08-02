6218fee7d72f17ba2e9059e8da296e1f1d0b8e3e
Anish Prakash <anish1.prakash@gmail.com>
2026-08-02 11:27:44 +0530

    Time: 0 ms (100%), Space: 8.5 MB (100%) - LeetHub

--- 0877-stone-game/0877-stone-game.c
+++ 0877-stone-game/0877-stone-game.c
@@ -1,18 +1,3 @@
 bool stoneGame(int* piles, int pilesSize) {
-    int dp[500];
-    
-    for (int i = 0; i < pilesSize; i++) {
-        dp[i] = piles[i];
-    }
-    
-    for (int len = 2; len <= pilesSize; len++) {
-        for (int i = 0; i <= pilesSize - len; i++) {
-            int j = i + len - 1;
-            int left = piles[i] - dp[i + 1];
-            int right = piles[j] - dp[i];
-            dp[i] = left > right ? left : right;
-        }
-    }
-    
-    return dp[0] > 0;
+    return true;
 }
