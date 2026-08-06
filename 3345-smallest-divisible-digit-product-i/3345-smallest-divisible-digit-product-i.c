498cd2108a3b4604a76c5e6e0877b090a6523afb
Anish Prakash <anish1.prakash@gmail.com>
2026-08-06 09:44:14 +0530

    Time: 0 ms (100%), Space: 9 MB (94%) - LeetHub

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.c
@@ -0,0 +1,14 @@
+int smallestNumber(int n, int t) {
+    while (1) {
+        int temp = n;
+        int prod = 1;
+        while (temp > 0) {
+            prod *= (temp % 10);
+            temp /= 10;
+        }
+        if (prod % t == 0) {
+            return n;
+        }
+        n++;
+    }
+}
