df81a53b9a7f09adc85c9b863848c05083e350ef
Anish Prakash <anish1.prakash@gmail.com>
2026-08-25 10:20:55 +0530

    Time: 0 ms (100%), Space: 9.7 MB (60.53%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.c
@@ -0,0 +1,16 @@
+int missingMultiple(int* nums, int numsSize, int k) {
+    int target = k;
+    while(1) {
+        int found = 0;
+        for(int i = 0; i < numsSize; i++) {
+            if(nums[i] == target) {
+                found = 1;
+                break;
+            }
+        }
+        if(!found) {
+            return target;
+        }
+        target += k;
+    }
+}
